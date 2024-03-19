b3f07a598bc8a62e5b012d6423122c7599999960
Mayur Marwade <mayumarwade899@gmail.com>
2024-03-19 20:19:10 +0530

    Image Hover Animation added

---
+++ ImageHoveringAnimation/script.js
@@ -0,0 +1,16 @@
+var element = document.querySelectorAll(".element");
+
+element.forEach((val) =>{
+    val.addEventListener("mouseenter", ()=> {
+        val.childNodes[3].style.opacity = 1;
+    });
+
+    val.addEventListener("mouseleave", ()=> {
+        val.childNodes[3].style.opacity = 0
+    });
+
+    val.addEventListener("mousemove", (dets)=> {
+        val.childNodes[3].style.left = dets.x + "px";
+        // val.childNodes[3].style.top = dets.y + "px";
+    })
+})
