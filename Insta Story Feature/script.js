328e7bed2d0a263d41d063304e06183273895dc5
Mayur Marwade <mayumarwade899@gmail.com>
2024-03-21 22:53:37 +0530

    Insta Story Feature added

---
+++ Insta Story Feature/script.js
@@ -0,0 +1,41 @@
+var arr = [
+    {dp: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9kZWx8ZW58MHx8MHx8fDA%3D",
+     story: "https://images.unsplash.com/photo-1564485377539-4af72d1f6a2f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG1vZGVsfGVufDB8fDB8fHww"
+    },
+
+    {dp: "https://images.unsplash.com/photo-1488161628813-04466f872be2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1vZGVsfGVufDB8fDB8fHww",
+     story: "https://plus.unsplash.com/premium_photo-1669703777695-f8052a432411?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fG1vZGVsfGVufDB8fDB8fHww"
+    },
+    
+    {dp: "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG1vZGVsfGVufDB8fDB8fHww",
+     story: "https://images.unsplash.com/photo-1562572159-4efc207f5aff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fG1vZGVsfGVufDB8fDB8fHww"
+    },
+
+    {dp: "https://images.unsplash.com/photo-1710866380940-7e0c7aa925b0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D",
+     story: "https://plus.unsplash.com/premium_photo-1674326713338-a7ff323c7d72?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNXx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+    {dp: "https://images.unsplash.com/photo-1696717597414-d706584f8128?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0OHx8fGVufDB8fHx8fA%3D%3D",
+     story: "https://images.unsplash.com/photo-1710839031473-27be50909585?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3N3x8fGVufDB8fHx8fA%3D%3D"
+    },
+]
+
+var stories = document.querySelector("#stories")
+var clutter = ""
+
+arr.forEach(function(elem, idx) {
+    clutter += `<div class="story">
+    <img id="${idx}" src="${elem.dp}" alt="">
+    </div>`
+})
+
+stories.innerHTML = clutter
+
+stories.addEventListener("click", (dets)=> {
+    document.querySelector("#fullScreen").style.display = "block"
+    document.querySelector("#fullScreen").style.backgroundImage = `url(${arr[dets.target.id].story})`
+
+    setTimeout(()=> {
+        document.querySelector("#fullScreen").style.display = "none"
+    }, 2000)
+})
